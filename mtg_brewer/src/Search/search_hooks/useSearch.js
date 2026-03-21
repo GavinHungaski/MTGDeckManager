1920d61f62c2961a7f93eb66790d9e6123b5559b
Gavin <ghungaski@gmail.com>
2026-03-21 15:38:28 -0400

    Added styling to parameter tray fo visual distinction

--- mtg_brewer/src/Search/search_hooks/useSearch.js
+++ mtg_brewer/src/Search/search_hooks/useSearch.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useRef } from "react";
 
 function buildQuery(searchText, colors, sortBy, extraFilters) {
   const parts = [];
