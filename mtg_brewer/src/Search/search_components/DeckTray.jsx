fd1f5a20c0a4e84de7185a2d743d3ad4ceed35dc
Gavin <ghungaski@gmail.com>
2026-03-21 23:28:27 -0400

    Finished styling deck tray

--- mtg_brewer/src/Search/search_components/DeckTray.jsx
+++ mtg_brewer/src/Search/search_components/DeckTray.jsx
@@ -7,28 +7,30 @@ function DeckTray() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className={`deck-tray ${isOpen ? "open" : ""}`}>
+    <>
       <button
-        className="deck-tray-btn"
+        className={`deck-tray-btn ${isOpen ? "open" : ""}`}
         onClick={() => {
           setIsOpen(!isOpen);
         }}
       >
         <span className="button-top">{isOpen ? ">" : "<"}</span>
       </button>
-      {decks.map((deck) => {
-        return (
-          <div className="deck-item" key={deck.id}>
-            <p>{deck.name}</p>
-            <img
-              className="commander-art"
-              src={deck.commander?.image || ""}
-              alt={deck.commander?.name || ""}
-            />
-          </div>
-        );
-      })}
-    </div>
+      <div className={`deck-tray ${isOpen ? "open" : ""}`}>
+        {decks.map((deck) => {
+          return (
+            <div className="deck-item" key={deck.id}>
+              <p>{deck.name}</p>
+              <img
+                className="commander-art"
+                src={deck.commander?.image || ""}
+                alt={deck.commander?.name || ""}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
