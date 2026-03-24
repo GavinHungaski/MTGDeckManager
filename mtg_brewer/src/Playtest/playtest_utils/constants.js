9d095006907d697d22aea7163adc397b9b68e75d
Gavin <ghungaski@gmail.com>
2026-03-23 23:59:40 -0400

    Added the scaffolding to the Playtest area with a reducer and state file to haandle state and all actions, deckpile, hud, and hand are pseudo implemented. Card draw works

--- mtg_brewer/src/Playtest/playtest_utils/constants.js
+++ mtg_brewer/src/Playtest/playtest_utils/constants.js
@@ -1,7 +1,7 @@
 import cardBack from "../../assets/card_back.jpg";
 
 export const HUD_HEIGHT = 80;
-export const HAND_HEIGHT = 290;
+export const HAND_HEIGHT = 300;
 
 export const CARD_WIDTH = 200;
 export const CARD_HEIGHT = 288;
