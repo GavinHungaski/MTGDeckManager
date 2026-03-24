b61158abe1505b5845301a994bd23b0cad9754d7
GavinHungaski <ghungaski@gmail.com>
2026-03-24 01:28:14 -0400

    refined card ratio, 206 x 288 is the correct one

--- mtg_brewer/src/Playtest/playtest_utils/constants.js
+++ mtg_brewer/src/Playtest/playtest_utils/constants.js
@@ -3,7 +3,7 @@ import cardBack from "../../assets/card_back.jpg";
 export const HUD_HEIGHT = 80;
 export const HAND_HEIGHT = 300;
 
-export const CARD_WIDTH = 200;
+export const CARD_WIDTH = 206;
 export const CARD_HEIGHT = 288;
 
 export const CARD_BACK_IMAGE = cardBack;
