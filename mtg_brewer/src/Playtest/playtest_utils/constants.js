6aa74353965a6af6b2fdbb245f90f06fcd939815
Gavin <ghungaski@gmail.com>
2026-04-06 15:46:39 -0400

    changed constants to all be connected to card width

--- mtg_brewer/src/Playtest/playtest_utils/constants.js
+++ mtg_brewer/src/Playtest/playtest_utils/constants.js
@@ -1,9 +1,9 @@
 import cardBack from "../../assets/card_back.jpg";
 
-export const HUD_HEIGHT = 80;
-export const HAND_HEIGHT = 300;
-
 export const CARD_WIDTH = 206;
-export const CARD_HEIGHT = 288;
+export const CARD_HEIGHT = CARD_WIDTH * 1.4;
+
+export const HAND_HEIGHT = CARD_HEIGHT + 10;
+export const HUD_HEIGHT = CARD_WIDTH * 0.388;
 
 export const CARD_BACK_IMAGE = cardBack;
