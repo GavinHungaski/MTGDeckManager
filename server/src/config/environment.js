f8ca798ee8b932b783552746545b90b4d6868b12
Gavin <ghungaski@gmail.com>
2026-05-14 14:40:43 -0400

    changed the rate limit to x3

--- server/src/config/environment.js
+++ server/src/config/environment.js
@@ -59,7 +59,7 @@ export const config = {
   // Rate Limiting
   rateLimit: {
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100 // limit each IP to 100 requests per windowMs
+    max: 300 // limit each IP to 300 requests per windowMs
   }
 };
 
